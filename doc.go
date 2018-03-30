5d4bb5ed6c8934679af5e2358d6efba5e3861ad1
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-03-30 18:16:32 +0200

    Add more precision on doc comment

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ Package rules provides a rules engine implementation.
 Usage of this package revolves around the concept of rulesets.
 
 A Ruleset can be represented as a list of rules that can be evaluated against a set of parameters given by a caller.
-Each rule is evaluated in order and if one matches with the given parameters it returns a result.
+Each rule is evaluated in order and if one matches with the given parameters it returns a result and the evaluation stops.
 All the rules of a ruleset always return the same type.
 
 A Store is capable of serving rulesets on demand to the Engine which evaluates them to return a result.
