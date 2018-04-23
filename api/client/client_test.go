160dedcf11849a13def6f9ddf4ba48c7e2c8cd34
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-04-23 16:04:09 +0200

    Add HTTP Client

---
+++ api/client/client_test.go
@@ -0,0 +1,46 @@
+package client_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/heetch/rules-engine/api/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClient(t *testing.T) {
+	t.Run("Error", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "some err"}`)
+		}))
+		defer ts.Close()
+
+		cli, err := client.NewClient(ts.URL)
+		require.NoError(t, err)
+
+		_, err = cli.ListRulesets(context.Background())
+		require.EqualError(t, err, "some err")
+	})
+
+	t.Run("ListRulesets", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.NotEmpty(t, r.Header.Get("User-Agent"))
+			assert.Equal(t, "application/json", r.Header.Get("Accept"))
+			fmt.Fprintf(w, `[{"name": "a"}]`)
+		}))
+		defer ts.Close()
+
+		cli, err := client.NewClient(ts.URL)
+		require.NoError(t, err)
+
+		rs, err := cli.ListRulesets(context.Background())
+		require.NoError(t, err)
+		require.Len(t, rs, 1)
+	})
+
+}
