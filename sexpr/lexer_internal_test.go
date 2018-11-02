b9f2fa00987a71aae3a8e994a4e1c009daeb80e8
Geoffrey John Teale <tealeg@stinky.fritz.box>
2018-11-02 14:26:48 +0100

    Add negative test cases for isWhitespace

--- sexpr/lexer_internal_test.go
+++ sexpr/lexer_internal_test.go
@@ -12,5 +12,13 @@ func TestIsWhitespace(t *testing.T) {
 	require.True(t, isWhitespace('\t'))
 	require.True(t, isWhitespace('\r'))
 	require.True(t, isWhitespace('\n'))
-
+	require.False(t, isWhitespace('-'))
+	require.False(t, isWhitespace('a'))
+	require.False(t, isWhitespace('"'))
+	require.False(t, isWhitespace('('))
+	require.False(t, isWhitespace(')'))
+	require.False(t, isWhitespace('_'))
+	require.False(t, isWhitespace('0'))
+	require.False(t, isWhitespace('#'))
+	require.False(t, isWhitespace(';'))
 }
