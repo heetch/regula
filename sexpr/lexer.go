3fac9a1f9d8faf8bb5f54daf04d921b627625aa9
Geoffrey John Teale <tealeg@stinky.fritz.box>
2018-11-02 16:18:31 +0100

    Document predicates

--- sexpr/lexer.go
+++ sexpr/lexer.go
@@ -14,22 +14,30 @@ const (
 	SYMBOL
 )
 
+// isWhitespace returns true if the rune is the first rune of a
+// Whitespace sequence.
 func isWhitespace(r rune) bool {
 	return r == ' ' || r == '\t' || r == '\r' || r == '\n'
 }
 
+// isLParen returns true if the rune is a left parenthesis
 func isLParen(r rune) bool {
 	return r == '('
 }
 
+// isRParen returns true if the rune is a right parenthesis
 func isRParen(r rune) bool {
 	return r == ')'
 }
 
+// isString returns true if the rune is a double quote indicating the
+// beginning of string
 func isString(r rune) bool {
 	return r == '"'
 }
 
+// isNumber returns true if the rune is an Arabic number or the minus
+// symbol, indicating the beginning of a number.
 func isNumber(r rune) bool {
 	// Note, although we allow a number to contain a decimal
 	// point, it can't start with one so we don't include that in
@@ -37,14 +45,20 @@ func isNumber(r rune) bool {
 	return r == '-' || (r >= '0' && r <= '9')
 }
 
+// isBool returns true if the rune is the # (hash or octothorpe)
+// indicating the beginning of a boolean.
 func isBool(r rune) bool {
 	return r == '#'
 }
 
+// isComment returns true if the rune is ; (semicolon) indicating the
+// beginning of a comment.
 func isComment(r rune) bool {
 	return r == ';'
 }
 
+// isSymbol returns true if no other special character type matches
+// the rune.
 func isSymbol(r rune) bool {
 	return !(isWhitespace(r) || isLParen(r) || isRParen(r) || isString(r) || isNumber(r) || isBool(r) || isComment(r))
 }
