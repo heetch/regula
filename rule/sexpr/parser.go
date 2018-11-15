0f0090bf846d01d2776b85db3b177b80f1d36c08
Geoffrey John Teale <tealeg@gmail.com>
2018-11-15 11:31:22 +0100

    Test we can construct a buffered Parser, scan and unscan

---
+++ rule/sexpr/parser.go
@@ -0,0 +1,30 @@
+package sexpr
+
+import "io"
+
+// Parser
+type Parser struct {
+	s        *Scanner
+	buf      lexicalElement
+	buffered bool
+}
+
+func NewParser(r io.Reader) *Parser {
+	return &Parser{s: NewScanner(r)}
+}
+
+// scan returns the next lexicalElement from the text to be parsed, or
+// the buffered element if unscan was called prior to scan.
+func (p *Parser) scan() (lexicalElement, error) {
+	var err error
+	if !p.buffered {
+		p.buf, err = p.s.Scan()
+	}
+	p.buffered = false
+	return p.buf, err
+}
+
+// unscan instructs the Parser to use the buffered value for the next call to scan
+func (p *Parser) unscan() {
+	p.buffered = true
+}
