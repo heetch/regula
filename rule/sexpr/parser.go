839196777827a9372bd8e5ab5160f388dd9af421
Geoffrey John Teale <tealeg@gmail.com>
2018-11-29 10:41:00 +0100

    Add the opMap to the parser definition.

---
+++ rule/sexpr/parser.go
@@ -0,0 +1,25 @@
+package sexpr
+
+// makeSymbolMap returns a mapOp with the full map of the built-in
+// symbols of our symbolic expression language to their implementation
+// as regula rule.Exprs.
+func makeSymbolMap() *mapOp {
+	sm = newMapOp()
+	sm.mapSymbol("=", "eq")
+	sm.mapSymbol("+", "add")
+	sm.mapSymbol("-", "sub")
+	sm.mapSymbol("*", "mult")
+	sm.mapSymbol("/", "div")
+	sm.mapSymbol("%", "mod")
+	sm.mapSymbol("in", "in")
+	sm.mapSymbol("and", "and")
+	sm.mapSymbol("or", "or")
+	sm.mapSymbol("not", "not")
+	sm.mapSymbol(">", "gt")
+	sm.mapSymbol(">=", "gte")
+	sm.mapSymbol("<", "lt")
+	sm.mapSymbol("<=", "lte")
+	sm.mapSymbol("hash", "hash")
+	sm.mapSymbol("percentile", "percentile")
+	return sm
+}
