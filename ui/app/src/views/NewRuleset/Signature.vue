a4ba901f468070fa6ad73e57fd6549cf81ce573c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-12-07 15:17:06 +0100

    Split NewRuleset page into multiple components

--- ui/app/src/views/NewRuleset/Signature.vue
+++ ui/app/src/views/NewRuleset/Signature.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-container class="new-ruleset ">
-    <h1 class="display-1">New ruleset</h1>
-
-    <v-form>
-
-      <v-card class="mt-3">
+    <v-card class="mt-3">
 
         <v-card-title primary-title>
           <div>
@@ -59,54 +54,11 @@
         </v-card-text>
 
       </v-card>
-
-      <v-card class="mt-3">
-
-        <v-card-title primary-title>
-          <div>
-            <h3 class="title">Rules</h3>
-          </div>
-        </v-card-title>
-
-        <v-card-text>
-            <!-- parameters  -->
-          <h3 class="subheading mb-3">Rule 1</h3>
-          <v-layout row wrap>
-            <v-flex xs12 sm8 class="pr-2">
-              <v-textarea
-                box
-                label="Code"
-                name="rule-1"
-                value="(#true)"
-              ></v-textarea>
-            </v-flex>
-            <v-flex xs12 sm4 class="pr-2">
-              <v-text-field
-                box
-                :rules="pathRules"
-                label="Result"
-                required
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-          <v-btn small fab color="secondary" class="ma-0 mt-2">
-            <v-icon dark>mdi-plus</v-icon>
-          </v-btn>
-
-        </v-card-text>
-
-      </v-card>
-
-      <div class="text-xs-right mt-3">
-        <v-btn
-          :disabled="!valid"
-          @click="submit"
-          color="primary"
-        >
-          create new ruleset
-        </v-btn>
-      </div>
-    </v-form>
-
-  </v-container>
 </template>
+
+<script>
+export default {
+  name: 'Signature',
+};
+</script>
+
