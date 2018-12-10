e537f231c44254e7f31792d37e8733b62adbea85
Geoffrey John Teale <tealeg@gmail.com>
2018-12-10 17:11:02 +0100

    Add IntToFloat operator and operator homogenisation.

--- rule/expr.go
+++ rule/expr.go
@@ -313,6 +313,50 @@ func (n *exprIn) Eval(params Params) (*Value, error) {
 	return BoolValue(false), nil
 }
 
+// exprIntToFloat is an operation that converts an Int64 type to a Float64 type.
+type exprIntToFloat struct {
+	operator
+}
+
+// newExprIntToFloat returns a pointer to a exprIntToFloat operator, initialised with a Contract.
+func newExprIntToFloat() *exprIntToFloat {
+	return &exprIntToFloat{
+		operator: operator{
+			contract: Contract{
+				OpCode:     "intToFloat",
+				ReturnType: FLOAT,
+				Terms: []Term{
+					{
+						Cardinality: ONE,
+						Type:        INTEGER,
+					},
+				},
+			},
+		},
+	}
+}
+
+// IntToFloat is an operator that converts as Float64 into an Int64 type.
+func IntToFloat(vN ...Expr) Expr {
+	e := newExprIntToFloat()
+	e.consumeOperands(vN...)
+	return e
+}
+
+// Eval will convert the operand provided to exprIntToFloat to a Float64Value.  Eval makes exprIntToFloat implement the Expr interface.
+func (n *exprIntToFloat) Eval(params Params) (*Value, error) {
+	op := n.operands[0]
+	val, err := op.Eval(params)
+	if err != nil {
+		return nil, err
+	}
+	iVal, err := strconv.ParseInt(val.Data, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return Float64Value(float64(iVal)), nil
+}
+
 // Param is an expression used to select a parameter passed during evaluation and return its corresponding value.
 type Param struct {
 	Kind string `json:"kind"`
