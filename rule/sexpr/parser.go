8949e8fe32da6ca880d856d9ddb8c6f10224eb0f
Geoffrey John Teale <tealeg@gmail.com>
2018-12-12 16:05:25 +0100

    Resolve conflict

--- rule/sexpr/parser.go
+++ rule/sexpr/parser.go
@@ -214,20 +214,7 @@ Loop:
 				// Just return the Number
 				break Loop
 			}
-			if err := opExpr.(rule.Operator).PushExpr(expr); err != nil {
-				return nil, newParserError(le, err)
-			}
-
-		case NUMBER:
-			expr, err := p.makeNumber(le)
-			if err != nil {
-				return nil, err
-			}
-			if !inOperator {
-				// Just return the Number
-				break Loop
-			}
-			if err := opExpr.(rule.Operator).PushExpr(expr); err != nil {
+			if err := opExpr.PushExpr(expr); err != nil {
 				return nil, newParserError(le, err)
 			}
 
