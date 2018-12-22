f9a8bcabda9906bb525c05332b146855ffd596e7
Geoffrey John Teale <tealeg@gmail.com>
2018-12-22 23:55:01 +0100

    Add failing test for posting new rulset from UI

--- ui/handler.go
+++ ui/handler.go
@@ -1,11 +1,19 @@
 package ui
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"path/filepath"
+	"strconv"
 
+	"github.com/heetch/regula"
 	reghttp "github.com/heetch/regula/http"
+	regrule "github.com/heetch/regula/rule"
+	"github.com/heetch/regula/rule/sexpr"
 	"github.com/heetch/regula/store"
 )
 
@@ -77,6 +85,7 @@ func newInternalHandler(service store.RulesetService) http.Handler {
 
 // Returns an http handler that lists all existing rulesets paths.
 func (h *internalHandler) rulesetsHandler() http.Handler {
+
 	type ruleset struct {
 		Path string `json:"path"`
 	}
@@ -86,23 +95,203 @@ func (h *internalHandler) rulesetsHandler() http.Handler {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var resp response
-		var token string
-
-		// run the loop at least once, no matter of the value of token
-		for i := 0; i == 0 || token != ""; i++ {
-			list, err := h.service.List(r.Context(), "", 100, token, true)
+		switch r.Method {
+		case "POST":
+			body, err := ioutil.ReadAll(r.Body)
 			if err != nil {
-				writeError(w, r, err, http.StatusInternalServerError)
+				writeError(w, r, err, http.StatusBadRequest)
+			}
+			nrr := &newRulesetRequest{}
+			err = json.Unmarshal(body, nrr)
+			if err != nil {
+				writeError(w, r, err, http.StatusBadRequest)
 				return
 			}
-
-			token = list.Continue
-			for _, rs := range list.Entries {
-				resp.Rulesets = append(resp.Rulesets, ruleset{Path: rs.Path})
+			rs, err := newRulesetFromRequest(nrr)
+			if err != nil {
+				writeError(w, r, err, http.StatusBadRequest)
+				return
 			}
+			_, err = h.service.Put(r.Context(), nrr.Path, rs)
+			if err != nil {
+				writeError(w, r, err, http.StatusBadRequest)
+				return
+			}
+			reghttp.EncodeJSON(w, r, nil, http.StatusOK)
+		case "GET":
+			var resp response
+			var token string
+
+			// run the loop at least once, no matter of the value of token
+			for i := 0; i == 0 || token != ""; i++ {
+				list, err := h.service.List(r.Context(), "", 100, token, true)
+				if err != nil {
+					writeError(w, r, err, http.StatusInternalServerError)
+					return
+				}
+
+				token = list.Continue
+				for _, rs := range list.Entries {
+					resp.Rulesets = append(resp.Rulesets, ruleset{Path: rs.Path})
+				}
+			}
+
+			reghttp.EncodeJSON(w, r, &resp, http.StatusOK)
 		}
 
-		reghttp.EncodeJSON(w, r, &resp, http.StatusOK)
 	})
+}
+
+type param map[string]string
+
+type signature struct {
+	Params     []param `json:"params"`
+	ReturnType string
+}
+
+type rule struct {
+	SExpr       string `json:"sExpr"`
+	ReturnValue string `json:"returnValue"`
+}
+
+type newRulesetRequest struct {
+	Path      string    `json:"path"`
+	Signature signature `json:"signature"`
+	Rules     []rule    `json:"rules"`
+}
+
+func convertParams(input []param) (sexpr.Parameters, error) {
+	parm := make(sexpr.Parameters)
+	for _, p := range input {
+		for k, v := range p {
+			t, err := regrule.TypeFromName(v)
+			if err != nil {
+				return nil, err
+			}
+			parm[k] = t
+		}
+	}
+	return parm, nil
+}
+
+func newRulesetFromRequest(nrr *newRulesetRequest) (*regula.Ruleset, error) {
+	var rules []*regrule.Rule
+	parm, err := convertParams(nrr.Signature.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	for n, rule := range nrr.Rules {
+		p := sexpr.NewParser(bytes.NewBufferString(rule.SExpr))
+		expr, err := p.Parse(parm)
+		if err != nil {
+			return nil, newRuleError(n+1, err)
+		}
+		val, err := makeValue(nrr.Signature.ReturnType, rule.ReturnValue)
+		if err != nil {
+			return nil, err
+		}
+		rules = append(rules, &regrule.Rule{
+			Expr:   expr,
+			Result: val,
+		})
+
+	}
+
+	return makeRuleset(nrr.Signature.ReturnType, rules...)
+}
+
+//
+func makeValue(returnType, returnValue string) (*regrule.Value, error) {
+	switch returnType {
+	case "string":
+		return regrule.StringValue(returnValue), nil
+	case "bool":
+		b, err := strconv.ParseBool(returnValue)
+		if err != nil {
+			return nil, err
+		}
+		return regrule.BoolValue(b), nil
+	case "int64":
+		i, err := strconv.ParseInt(returnValue, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return regrule.Int64Value(i), nil
+	case "float64":
+		f, err := strconv.ParseFloat(returnValue, 64)
+		if err != nil {
+			return nil, err
+		}
+		return regrule.Float64Value(f), nil
+	}
+	return nil, fmt.Errorf("Invalid return type %q", returnType)
+}
+
+func makeRuleset(returnType string, rules ...*regrule.Rule) (*regula.Ruleset, error) {
+	switch returnType {
+	case "string":
+		return regula.NewStringRuleset(rules...)
+	case "bool":
+		return regula.NewBoolRuleset(rules...)
+	case "int64":
+		return regula.NewInt64Ruleset(rules...)
+	case "float64":
+		return regula.NewFloat64Ruleset(rules...)
+	}
+	return nil, fmt.Errorf("Invalid return type %q", returnType)
+}
+
+type RuleError struct {
+	ruleNum int
+	err     error
+}
+
+func newRuleError(ruleNum int, qerr error) *RuleError {
+	return &RuleError{ruleNum: ruleNum, err: err}
+}
+
+//
+func (re *RuleError) Error() string {
+	pe, ok := re.err.(sexpr.ParserError)
+	if !ok {
+		return re.err.Error()
+	}
+	return fmt.Sprintf("%s in rule %d: %s", pe.ErrorType, re.ruleNum, pe.Msg)
+}
+
+//
+func (re *RuleError) MarshalJSON() ([]byte, error) {
+	type field struct {
+		Path  []string `json:"path"`
+		Error struct {
+			Message string `json:"message"`
+			Line    int    `json:"line"`
+			Char    int    `json:"char"`
+			AbsChar int    `json:"absChar"`
+		} `json:"error"`
+	}
+
+	var err struct {
+		Error  string  `json:"error"`
+		Fields []field `json:"fields"`
+	}
+	errMsg := re.Error()
+	err.Error = errMsg
+	pe, ok := re.err.(sexpr.ParserError)
+	if !ok {
+		return json.Marshal(err)
+	}
+	err.Fields = []field{
+		{
+			Path: []string{"rules", strconv.Itoa(err.ruleNum), "sExpr"},
+			Error: {
+				Message: errMsg,
+				Line:    pe.StartLine,
+				Char:    pe.StartCharInLine,
+				AbsChar: StartChar,
+			},
+		},
+	}
+	return json.Marshal(err)
 }
--- ui/handler_test.go
+++ ui/handler_test.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/heetch/regula/mock"
@@ -21,52 +22,103 @@ func doRequest(h http.Handler, method, path string, body io.Reader) *httptest.Re
 	return w
 }
 
+func TestPOST(t *testing.T) {
+	s := new(mock.RulesetService)
+	rec := doRequest(NewHandler(s, ""), "POST", "/i/rulesets/", strings.NewReader(`{
+    "path": "Path1",
+    "signature": {
+        "params": [
+            {
+                "foo": "string"
+            }
+        ],
+        "returnType": "string"
+    },
+    "rules": [
+        {
+            "sExpr": "(= 1 1",
+            "returnValue": "wibble"
+        }
+    ]
+}`))
+	require.Equal(t, http.StatusBadRequest, rec.Code)
+	body := rec.Body.String()
+	t.Logf("JSON: %q", body)
+	require.JSONEq(t, `{
+	    "error": "unexpected end of file whilst parsing rule",
+	    "fields": [
+		{
+		    "path": ["rules", "2", "sExpr"],
+		    "error": {
+			"message": "unexpected end of file whilst parsing rule",
+			"line": 10,
+			"char": 11,
+			"absChar": 120
+		    }
+		},
+	    ]
+	}
+	`, body)
+}
+
 func TestInternalHandler(t *testing.T) {
 	t.Run("Rulesets", func(t *testing.T) {
-		// this test checks if the handler deals with pagination correctly
-		// and returns the right payload
-		t.Run("OK", func(t *testing.T) {
-			s := new(mock.RulesetService)
+		// POSTing to the ruleset endpoint attempts to create a new ruleset
+		// t.Run("POST", func(t *testing.T) {
+		// 	// Posting invalid ruleset data returns an error.
+		//      t.Run("Validation Error", func(t *testing.T) {
 
-			// simulate a two page result
-			s.ListFn = func(ctx context.Context, _ string, limit int, token string, pathsOnly bool) (*store.RulesetEntries, error) {
-				var entries store.RulesetEntries
+		// 	})
+		// })
 
-				switch token {
-				case "":
-					for i := 0; i < 2; i++ {
-						entries.Entries = append(entries.Entries, store.RulesetEntry{
-							Path: fmt.Sprintf("Path%d", i),
-						})
+		// GETting the ruleset endpoint returns a list of rulsets
+		t.Run("GET", func(t *testing.T) {
+			// this test checks if the handler deals with pagination correctly
+			// and returns the right payload
+			t.Run("OK", func(t *testing.T) {
+				s := new(mock.RulesetService)
+
+				// simulate a two page result
+				s.ListFn = func(ctx context.Context, _ string, limit int, token string, pathsOnly bool) (*store.RulesetEntries, error) {
+					var entries store.RulesetEntries
+
+					switch token {
+					case "":
+						for i := 0; i < 2; i++ {
+							entries.Entries = append(entries.Entries, store.RulesetEntry{
+								Path: fmt.Sprintf("Path%d", i),
+							})
+						}
+
+						entries.Continue = "continue"
+					case "continue":
+						for i := 2; i < 3; i++ {
+							entries.Entries = append(entries.Entries, store.RulesetEntry{
+								Path: fmt.Sprintf("Path%d", i),
+							})
+						}
+						entries.Continue = ""
 					}
 
-					entries.Continue = "continue"
-				case "continue":
-					for i := 2; i < 3; i++ {
-						entries.Entries = append(entries.Entries, store.RulesetEntry{
-							Path: fmt.Sprintf("Path%d", i),
-						})
-					}
-					entries.Continue = ""
+					return &entries, nil
 				}
 
-				return &entries, nil
-			}
+				rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
+				require.Equal(t, http.StatusOK, rec.Code)
+				require.JSONEq(t, `{"rulesets": [{"path": "Path0"},{"path": "Path1"},{"path": "Path2"}]}`, rec.Body.String())
+			})
 
-			rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
-			require.Equal(t, http.StatusOK, rec.Code)
-			require.JSONEq(t, `{"rulesets": [{"path": "Path0"},{"path": "Path1"},{"path": "Path2"}]}`, rec.Body.String())
-		})
+			// this test checks if the handler returns a 500 if a random error occurs in the ruleset service.
+			t.Run("Error", func(t *testing.T) {
+				s := new(mock.RulesetService)
+				s.ListFn = func(ctx context.Context, _ string, limit int, token string, pathsOnly bool) (*store.RulesetEntries, error) {
+					return nil, errors.New("some error")
+				}
 
-		// this test checks if the handler returns a 500 if a random error occurs in the ruleset service.
-		t.Run("Error", func(t *testing.T) {
-			s := new(mock.RulesetService)
-			s.ListFn = func(ctx context.Context, _ string, limit int, token string, pathsOnly bool) (*store.RulesetEntries, error) {
-				return nil, errors.New("some error")
-			}
+				rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
+				require.Equal(t, http.StatusInternalServerError, rec.Code)
+			})
 
-			rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
-			require.Equal(t, http.StatusInternalServerError, rec.Code)
 		})
 	})
 }
