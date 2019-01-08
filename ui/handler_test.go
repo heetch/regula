e6e44da9c9417d57946adb413429027d7bf5bfca
Geoffrey John Teale <tealeg@gmail.com>
2019-01-08 12:43:19 +0100

    Test succesful POST of a new ruleset to the mock backend

--- ui/handler_test.go
+++ ui/handler_test.go
@@ -22,9 +22,10 @@ func doRequest(h http.Handler, method, path string, body io.Reader) *httptest.Re
 	return w
 }
 
-func TestPOST(t *testing.T) {
+func TestPOSTNewRulesetWithParserError(t *testing.T) {
 	s := new(mock.RulesetService)
-	rec := doRequest(NewHandler(s, ""), "POST", "/i/rulesets/", strings.NewReader(`{
+	rec := doRequest(NewHandler(s, ""), "POST", "/i/rulesets/",
+		strings.NewReader(`{
     "path": "Path1",
     "signature": {
         "params": [
@@ -58,62 +59,74 @@ func TestPOST(t *testing.T) {
     ]
 }
 `, body)
+	require.Equal(t, 0, s.PutCount)
+}
+
+func TestPOSTNewRuleset(t *testing.T) {
+	s := new(mock.RulesetService)
+	rec := doRequest(NewHandler(s, ""), "POST", "/i/rulesets/",
+		strings.NewReader(`{
+    "path": "Path1",
+    "signature": {
+        "params": [
+            {
+                "foo": "string"
+            }
+        ],
+        "returnType": "string"
+    },
+    "rules": [
+        {
+            "sExpr": "(= 1 1)",
+            "returnValue": "wibble"
+        }
+    ]
+}`))
+	require.Equal(t, http.StatusOK, rec.Code)
+	require.Equal(t, 1, s.PutCount)
 }
 
 func TestInternalHandler(t *testing.T) {
-	t.Run("Rulesets", func(t *testing.T) {
-		// POSTing to the ruleset endpoint attempts to create a new ruleset
-		// t.Run("POST", func(t *testing.T) {
-		// 	// Posting invalid ruleset data returns an error.
-		//      t.Run("Validation Error", func(t *testing.T) {
+	// this test checks if the handler deals with pagination correctly
+	// and returns the right payload
+	t.Run("OK", func(t *testing.T) {
+		s := new(mock.RulesetService)
 
-		// 	})
-		// })
+		// simulate a two page result
+		s.ListFn = func(ctx context.Context, _ string, opt *store.ListOptions) (*store.RulesetEntries, error) {
+			var entries store.RulesetEntries
 
-		// GETting the ruleset endpoint returns a list of rulsets
-		t.Run("GET", func(t *testing.T) {
-			// this test checks if the handler deals with pagination correctly
-			// and returns the right payload
-			t.Run("OK", func(t *testing.T) {
-				s := new(mock.RulesetService)
-
-				// simulate a two page result
-				s.ListFn = func(ctx context.Context, _ string, opt *store.ListOptions) (*store.RulesetEntries, error) {
-					var entries store.RulesetEntries
-
-					switch opt.ContinueToken {
-					case "":
-						for i := 0; i < 2; i++ {
-							entries.Entries = append(entries.Entries, store.RulesetEntry{
-								Path: fmt.Sprintf("Path%d", i),
-							})
-						}
-
-						entries.Continue = "continue"
-					case "continue":
-						for i := 2; i < 3; i++ {
-							entries.Entries = append(entries.Entries, store.RulesetEntry{
-								Path: fmt.Sprintf("Path%d", i),
-							})
-						}
-						entries.Continue = ""
-					}
-
-					return &entries, nil
+			switch opt.ContinueToken {
+			case "":
+				for i := 0; i < 2; i++ {
+					entries.Entries = append(entries.Entries, store.RulesetEntry{
+						Path: fmt.Sprintf("Path%d", i),
+					})
 				}
 
-				rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
-				require.Equal(t, http.StatusOK, rec.Code)
-				require.JSONEq(t, `{"rulesets": [{"path": "Path0"},{"path": "Path1"},{"path": "Path2"}]}`, rec.Body.String())
-			})
-
-			// this test checks if the handler returns a 500 if a random error occurs in the ruleset service.
-			t.Run("Error", func(t *testing.T) {
-				s := new(mock.RulesetService)
-				s.ListFn = func(ctx context.Context, _ string, opt *store.ListOptions) (*store.RulesetEntries, error) {
-					return nil, errors.New("some error")
+				entries.Continue = "continue"
+			case "continue":
+				for i := 2; i < 3; i++ {
+					entries.Entries = append(entries.Entries, store.RulesetEntry{
+						Path: fmt.Sprintf("Path%d", i),
+					})
 				}
-			})
-		})
+				entries.Continue = ""
+			}
+
+			return &entries, nil
+		}
+
+		rec := doRequest(NewHandler(s, ""), "GET", "/i/rulesets/", nil)
+		require.Equal(t, http.StatusOK, rec.Code)
+		require.JSONEq(t, `{"rulesets": [{"path": "Path0"},{"path": "Path1"},{"path": "Path2"}]}`, rec.Body.String())
+	})
+
+	// this test checks if the handler returns a 500 if a random error occurs in the ruleset service.
+	t.Run("Error", func(t *testing.T) {
+		s := new(mock.RulesetService)
+		s.ListFn = func(ctx context.Context, _ string, opt *store.ListOptions) (*store.RulesetEntries, error) {
+			return nil, errors.New("some error")
+		}
 	})
 }
