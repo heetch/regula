05c1d062fa84e4ed2c8b74e72291a2ae3b6fa27b
Geoffrey John Teale <tealeg@gmail.com>
2019-01-11 15:16:30 +0100

    Test float64 addition

--- rule/math_expr_test.go
+++ rule/math_expr_test.go
@@ -17,7 +17,15 @@ func TestAdd(t *testing.T) {
 		val, err := add.Eval(params)
 		require.NoError(t, err)
 		require.True(t, val.Same(rule.Int64Value(3)))
-
+	})
+	t.Run("Eval/Float64/OK", func(t *testing.T) {
+		n1 := rule.Float64Value(1.1)
+		n2 := rule.Float64Value(2.2)
+		params := regula.Params{}
+		add := rule.Add(n1, n2)
+		val, err := add.Eval(params)
+		require.NoError(t, err)
+		require.True(t, val.Same(rule.Float64Value(3.3)))
 	})
 
 }
