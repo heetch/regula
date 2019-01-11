18774b92c164bccb5814bed2f9f27bd9db359d6e
Geoffrey John Teale <tealeg@gmail.com>
2019-01-11 16:59:41 +0100

    Create Sub operator

--- rule/math_expr.go
+++ rule/math_expr.go
@@ -78,3 +78,100 @@ func (n *exprAdd) Eval(params Params) (*Value, error) {
 	}
 	return n.int64Add(params)
 }
+
+type exprSub struct {
+	operator
+}
+
+func newExprSub() *exprSub {
+	return &exprSub{
+		operator: operator{
+			contract: Contract{
+				OpCode:     "sub",
+				ReturnType: NUMBER,
+				Terms: []Term{
+					{
+						Type:        NUMBER,
+						Cardinality: MANY,
+						Min:         2,
+					},
+				},
+			},
+		},
+	}
+}
+
+// Sub creates an expression that takes at least two operands, which
+// must evaluate to either Float64Value or Int64Value, and returns the
+// value of subtracting the 2nd value from the first. Any values
+// beyond the second one are subtracted from the preceding result
+// until we reach the end of the values.
+func Sub(vN ...Expr) Expr {
+	e := newExprSub()
+	e.consumeOperands(vN...)
+	return e
+}
+
+func (n *exprSub) float64Sub(params Params) (*Value, error) {
+	o0 := n.operands[0]
+	v0, err := o0.Eval(params)
+	if err != nil {
+		return nil, err
+	}
+
+	f0, err := strconv.ParseFloat(v0.Data, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 1; i < len(n.operands); i++ {
+		o := n.operands[i]
+		v, err := o.Eval(params)
+		if err != nil {
+			return nil, err
+		}
+		f, err := strconv.ParseFloat(v.Data, 64)
+		if err != nil {
+			return nil, err
+		}
+		f0 = f0 - f
+	}
+	return Float64Value(f0), nil
+}
+
+func (n *exprSub) int64Sub(params Params) (*Value, error) {
+	o0 := n.operands[0]
+	v0, err := o0.Eval(params)
+	if err != nil {
+		return nil, err
+	}
+
+	i0, err := strconv.ParseInt(v0.Data, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	for j := 1; j < len(n.operands); j++ {
+		o := n.operands[j]
+		v, err := o.Eval(params)
+		if err != nil {
+			return nil, err
+		}
+		i, err := strconv.ParseInt(v.Data, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		i0 = i0 - i
+	}
+	return Int64Value(i0), nil
+}
+
+// Eval makes exprSub comply with the Expr interface.
+func (n *exprSub) Eval(params Params) (*Value, error) {
+	// The ReturnType will be set to the concrete type that
+	// matches all the arguments by homogenisation.
+	if n.operator.Contract().ReturnType == FLOAT {
+		return n.float64Sub(params)
+	}
+	return n.int64Sub(params)
+}
--- rule/math_expr_test.go
+++ rule/math_expr_test.go
@@ -27,5 +27,26 @@ func TestAdd(t *testing.T) {
 		require.NoError(t, err)
 		require.True(t, val.Same(rule.Float64Value(3.3)))
 	})
+}
+
+func TestSub(t *testing.T) {
+	t.Run("Eval/Int64/OK", func(t *testing.T) {
+		n1 := rule.Int64Value(1)
+		n2 := rule.Int64Value(1)
+		params := regula.Params{}
+		add := rule.Sub(n1, n2)
+		val, err := add.Eval(params)
+		require.NoError(t, err)
+		require.True(t, val.Same(rule.Int64Value(0)))
+	})
+	t.Run("Eval/Float64/OK", func(t *testing.T) {
+		n1 := rule.Float64Value(2.2)
+		n2 := rule.Float64Value(1.1)
+		params := regula.Params{}
+		add := rule.Sub(n1, n2)
+		val, err := add.Eval(params)
+		require.NoError(t, err)
+		require.True(t, val.Same(rule.Float64Value(1.1)))
+	})
 
 }
