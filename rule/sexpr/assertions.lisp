94f6797faaabc633625065856338db4f59e4c2ce
Geoffrey John Teale <tealeg@gmail.com>
2019-01-14 05:52:42 +0100

    Fix typos in assertions.lisp comments

--- rule/sexpr/assertions.lisp
+++ rule/sexpr/assertions.lisp
@@ -1,6 +1,6 @@
 ;; This file contains a number of assertions about our s-expression language.
 ;; These assertions are run as part of the normal Go test suits by the test case
-;; TestLispFileAssertions.
+;; TestLispFileAssertions in parser_test.go.
 ;; 
 ;; Each assertion must result in boolean value, if it True, then the test passes.
 ;; If it is False the test fails.
@@ -13,7 +13,7 @@
 (= 10.6 (+ 1 2.2 3 4.4))        ; Mixed Addition
 (= -3 (+ -2 -1))		; Addition of negative integers
 (= -4 (+ 5 -9))			; Addition of mixed sign integers
-(= -12.6 (+ 22.4 -35))          ; Addition of mixed precison and mixed sign
+(= -12.6 (+ 22.4 -35))          ; Addition of mixed precision and mixed sign
 (= 0 (- 1 1))			; Subtraction of integers
 (= 1.1 (- 2.2 1.1))             ; Subtraction of floats
 (= 1.0 (- 10.6 4.4 3 2.2))      ; Mixed subtraction
@@ -24,8 +24,8 @@
 (= 6.0 (- -4.1 -10.1))		; Subtraction of negatives floats
 (= 99 (* 11 9))			; Multiplication of integers
 (= 146.8944 (* 12.12 12.12))	; Multiplication of floats
-(= 220 (* -22 -10))		; Multiplicaton of negative integers
-(= 475.6 (* -20.5 -23.2))	; Multiplicatoin of negative floats
+(= 220 (* -22 -10))		; Multiplication of negative integers
+(= 475.6 (* -20.5 -23.2))	; Multiplication of negative floats
 (= -220 (* 22 -10))		; Multiplication of mixed sign integers
 (= -475.6 (* 20.5 -23.2))	; Multiplication of mixed sign floats
 (= 1 (/ 10 10))			; Integer division
