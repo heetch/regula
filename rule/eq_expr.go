5fa5fe784e1de033844157df0f271ce11cfcb7ac
Geoffrey John Teale <tealeg@gmail.com>
2019-01-24 11:20:14 +0100

    Refactor comparator logic somewhat.

--- rule/eq_expr.go
+++ rule/eq_expr.go
@@ -104,115 +104,19 @@ func LT(vN ...Expr) Expr {
 	return e
 }
 
-// perform a less-than comparison on a sequence of integers
-func (n *exprLT) integerLT(params Params) (*Value, error) {
-	var i0, i1 int64
-	var err error
-
-	i0, err = exprToInt64(n.operands[0], params)
-	if err != nil {
-		return nil, err
-	}
-	for j := 1; j < len(n.operands); j++ {
-		i1, err = exprToInt64(n.operands[j], params)
-		if err != nil {
-			return nil, err
-		}
-		if i0 >= i1 {
-			return BoolValue(false), nil
-		}
-		i0 = i1
-	}
-	return BoolValue(true), nil
-}
-
-// perform a less-than comparison on a sequence of floats
-func (n *exprLT) floatLT(params Params) (*Value, error) {
-	var f0, f1 float64
-	var err error
-
-	f0, err = exprToFloat64(n.operands[0], params)
-	if err != nil {
-		return nil, err
-	}
-	for j := 1; j < len(n.operands); j++ {
-		f1, err = exprToFloat64(n.operands[j], params)
-		if err != nil {
-			return nil, err
-		}
-		if f0 >= f1 {
-			return BoolValue(false), nil
-		}
-		f0 = f1
-	}
-	return BoolValue(true), nil
-}
-
-// perform a less-than comparison on a sequence of bools
-func (n *exprLT) boolLT(params Params) (*Value, error) {
-	var b0, b1 bool
-	var err error
-
-	if len(n.operands) > 2 {
-		// We can't have greater than 2 operands and maintain
-		// an inequality with a binary choice.
-		return BoolValue(false), nil
-	}
-
-	b0, err = exprToBool(n.operands[0], params)
-	if err != nil {
-		return nil, err
-	}
-	if b0 {
-		// If b0 is True then it's not less than b1, and we can be done already.
-		return BoolValue(false), nil
-	}
-	b1, err = exprToBool(n.operands[1], params)
-	if err != nil {
-		return nil, err
-	}
-	if !b1 {
-		// If b1 is False then b0 can't be less than it..
-		return BoolValue(false), nil
-	}
-	return BoolValue(true), nil
-}
-
-// perform a less-than comparison on a sequence of strings
-func (n *exprLT) stringLT(params Params) (*Value, error) {
-	var s0, s1 string
-	var err error
-
-	s0, err = exprToString(n.operands[0], params)
-	if err != nil {
-		return nil, err
-	}
-	for j := 1; j < len(n.operands); j++ {
-		s1, err = exprToString(n.operands[j], params)
-		if err != nil {
-			return nil, err
-		}
-		if s0 >= s1 {
-			return BoolValue(false), nil
-		}
-		s0 = s1
-	}
-	return BoolValue(true), nil
-}
-
 func (n *exprLT) Eval(params Params) (*Value, error) {
 	// Because of homogenisation during Parsing we know that all
 	// operands have the same type.
 	t := n.operands[0].Contract().ReturnType
 	switch t {
 	case INTEGER:
-		return n.integerLT(params)
+		return integerLT(n.operands, params)
 	case FLOAT:
-		return n.floatLT(params)
+		return floatLT(n.operands, params)
 	case BOOLEAN:
-		return n.boolLT(params)
+		return boolLT(n.operands, params)
 	case STRING:
-		return n.stringLT(params)
+		return stringLT(n.operands, params)
 	}
 	// This case should be unreachable if the parser is working correctly!
 	panic(fmt.Sprintf("subexpression evaluated to impossible type %q", t))
@@ -253,17 +157,39 @@ func GT(vN ...Expr) Expr {
 	return e
 }
 
+func (n *exprGT) Eval(params Params) (*Value, error) {
+	// Because of homogenisation during Parsing we know that all
+	// operands have the same type.
+	t := n.operands[0].Contract().ReturnType
+	switch t {
+	case INTEGER:
+		return integerGT(n.operands, params)
+	case FLOAT:
+		return floatGT(n.operands, params)
+	case BOOLEAN:
+		return boolGT(n.operands, params)
+	case STRING:
+		return stringGT(n.operands, params)
+	}
+	// This case should be unreachable if the parser is working correctly!
+	panic(fmt.Sprintf("subexpression evaluated to impossible type %q", t))
+}
+
+/////////////////////////////////
+// Underlying type comparators //
+/////////////////////////////////
+
 // perform a greater-than comparison on a sequence of integers
-func (n *exprGT) integerGT(params Params) (*Value, error) {
+func integerGT(operands []Expr, params Params) (*Value, error) {
 	var i0, i1 int64
 	var err error
 
-	i0, err = exprToInt64(n.operands[0], params)
+	i0, err = exprToInt64(operands[0], params)
 	if err != nil {
 		return nil, err
 	}
-	for j := 1; j < len(n.operands); j++ {
-		i1, err = exprToInt64(n.operands[j], params)
+	for j := 1; j < len(operands); j++ {
+		i1, err = exprToInt64(operands[j], params)
 		if err != nil {
 			return nil, err
 		}
@@ -276,16 +202,16 @@ func (n *exprGT) integerGT(params Params) (*Value, error) {
 }
 
 // perform a greater-than comparison on a sequence of floats
-func (n *exprGT) floatGT(params Params) (*Value, error) {
+func floatGT(operands []Expr, params Params) (*Value, error) {
 	var f0, f1 float64
 	var err error
 
-	f0, err = exprToFloat64(n.operands[0], params)
+	f0, err = exprToFloat64(operands[0], params)
 	if err != nil {
 		return nil, err
 	}
-	for j := 1; j < len(n.operands); j++ {
-		f1, err = exprToFloat64(n.operands[j], params)
+	for j := 1; j < len(operands); j++ {
+		f1, err = exprToFloat64(operands[j], params)
 		if err != nil {
 			return nil, err
 		}
@@ -298,17 +224,17 @@ func (n *exprGT) floatGT(params Params) (*Value, error) {
 }
 
 // perform a greater-than comparison on a sequence of bools
-func (n *exprGT) boolGT(params Params) (*Value, error) {
+func boolGT(operands []Expr, params Params) (*Value, error) {
 	var b0, b1 bool
 	var err error
 
-	if len(n.operands) > 2 {
+	if len(operands) > 2 {
 		// We can't have greater than 2 operands and maintain
 		// an inequality with a binary choice.
 		return BoolValue(false), nil
 	}
 
-	b0, err = exprToBool(n.operands[0], params)
+	b0, err = exprToBool(operands[0], params)
 	if err != nil {
 		return nil, err
 	}
@@ -316,7 +242,7 @@ func (n *exprGT) boolGT(params Params) (*Value, error) {
 		// If b0 is False then it's not greater than b1, and we can be done already.
 		return BoolValue(false), nil
 	}
-	b1, err = exprToBool(n.operands[1], params)
+	b1, err = exprToBool(operands[1], params)
 	if err != nil {
 		return nil, err
 	}
@@ -328,16 +254,16 @@ func (n *exprGT) boolGT(params Params) (*Value, error) {
 }
 
 // perform a greater-than comparison on a sequence of strings
-func (n *exprGT) stringGT(params Params) (*Value, error) {
+func stringGT(operands []Expr, params Params) (*Value, error) {
 	var s0, s1 string
 	var err error
 
-	s0, err = exprToString(n.operands[0], params)
+	s0, err = exprToString(operands[0], params)
 	if err != nil {
 		return nil, err
 	}
-	for j := 1; j < len(n.operands); j++ {
-		s1, err = exprToString(n.operands[j], params)
+	for j := 1; j < len(operands); j++ {
+		s1, err = exprToString(operands[j], params)
 		if err != nil {
 			return nil, err
 		}
@@ -349,20 +275,98 @@ func (n *exprGT) stringGT(params Params) (*Value, error) {
 	return BoolValue(true), nil
 }
 
-func (n *exprGT) Eval(params Params) (*Value, error) {
-	// Because of homogenisation during Parsing we know that all
-	// operands have the same type.
-	t := n.operands[0].Contract().ReturnType
-	switch t {
-	case INTEGER:
-		return n.integerGT(params)
-	case FLOAT:
-		return n.floatGT(params)
-	case BOOLEAN:
-		return n.boolGT(params)
-	case STRING:
-		return n.stringGT(params)
+// perform a less-than comparison on a sequence of integers
+func integerLT(operands []Expr, params Params) (*Value, error) {
+	var i0, i1 int64
+	var err error
+
+	i0, err = exprToInt64(operands[0], params)
+	if err != nil {
+		return nil, err
 	}
-	// This case should be unreachable if the parser is working correctly!
-	panic(fmt.Sprintf("subexpression evaluated to impossible type %q", t))
+	for j := 1; j < len(operands); j++ {
+		i1, err = exprToInt64(operands[j], params)
+		if err != nil {
+			return nil, err
+		}
+		if i0 >= i1 {
+			return BoolValue(false), nil
+		}
+		i0 = i1
+	}
+	return BoolValue(true), nil
+}
+
+// perform a less-than comparison on a sequence of floats
+func floatLT(operands []Expr, params Params) (*Value, error) {
+	var f0, f1 float64
+	var err error
+
+	f0, err = exprToFloat64(operands[0], params)
+	if err != nil {
+		return nil, err
+	}
+	for j := 1; j < len(operands); j++ {
+		f1, err = exprToFloat64(operands[j], params)
+		if err != nil {
+			return nil, err
+		}
+		if f0 >= f1 {
+			return BoolValue(false), nil
+		}
+		f0 = f1
+	}
+	return BoolValue(true), nil
+}
+
+// perform a less-than comparison on a sequence of bools
+func boolLT(operands []Expr, params Params) (*Value, error) {
+	var b0, b1 bool
+	var err error
+
+	if len(operands) > 2 {
+		// We can't have greater than 2 operands and maintain
+		// an inequality with a binary choice.
+		return BoolValue(false), nil
+	}
+
+	b0, err = exprToBool(operands[0], params)
+	if err != nil {
+		return nil, err
+	}
+	if b0 {
+		// If b0 is True then it's not less than b1, and we can be done already.
+		return BoolValue(false), nil
+	}
+	b1, err = exprToBool(operands[1], params)
+	if err != nil {
+		return nil, err
+	}
+	if !b1 {
+		// If b1 is False then b0 can't be less than it..
+		return BoolValue(false), nil
+	}
+	return BoolValue(true), nil
+}
+
+// perform a less-than comparison on a sequence of strings
+func stringLT(operands []Expr, params Params) (*Value, error) {
+	var s0, s1 string
+	var err error
+
+	s0, err = exprToString(operands[0], params)
+	if err != nil {
+		return nil, err
+	}
+	for j := 1; j < len(operands); j++ {
+		s1, err = exprToString(operands[j], params)
+		if err != nil {
+			return nil, err
+		}
+		if s0 >= s1 {
+			return BoolValue(false), nil
+		}
+		s0 = s1
+	}
+	return BoolValue(true), nil
 }
