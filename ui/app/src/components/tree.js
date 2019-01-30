676fe6488774941c18804e695afa4271360107da
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-01-30 12:28:36 +0100

    Add comments on the tree.js file

--- ui/app/src/components/tree.js
+++ ui/app/src/components/tree.js
@@ -1,3 +1,8 @@
+// buildTree is a recursive function that takes a path split into chunks
+// and returns a tree:
+//
+// For a given path 'a/b/c' it must return the following tree
+// {name: 'a', children: [{ name: 'b', children: [ { name: 'c', path: 'a/b/c' } ] }] }
 export const buildTree = (name = '', parents = [], rest = []) => ({
   name,
   ...(rest.length === 0 && { path: [...parents, name].join('/') }),
@@ -6,7 +11,8 @@ export const buildTree = (name = '', parents = [], rest = []) => ({
   }),
 });
 
-const mergeDupNodes = (trees = []) => {
+// mergeTrees takes a list of trees and merges them into a single one.
+const mergeTrees = (trees = []) => {
   const sorted = trees.sort((a, b) => a.name.localeCompare(b.name));
   let i = 0;
   let prev = {};
@@ -18,7 +24,7 @@ const mergeDupNodes = (trees = []) => {
       if (path) {
         prev.path = path;
       }
-      prev.children = mergeDupNodes([...(prev.children || []), ...(sorted[i].children || [])]);
+      prev.children = mergeTrees([...(prev.children || []), ...(sorted[i].children || [])]);
     } else {
       prev = sorted[i];
       list.push(sorted[i]);
@@ -30,11 +36,15 @@ const mergeDupNodes = (trees = []) => {
   return list;
 };
 
+// rulesetsToTree takes a list of rulesets and returns a tree compatible
+// with the Vuetify Treeview component format.
+// In order to do that, it must split the paths, generate several trees
+// then merge these trees into a single one and return it.
 export const rulesetsToTree = (rulesets = []) => {
   const trees = rulesets.map(({ path }) => {
     const chunks = path.split('/');
     return buildTree(chunks[0], [], chunks.slice(1));
   });
 
-  return mergeDupNodes(trees);
+  return mergeTrees(trees);
 };
