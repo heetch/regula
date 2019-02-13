28ed6e4b6be0e5d1169475315da0a6a98781c239
Geoffrey John Teale <tealeg@gmail.com>
2019-02-13 14:10:27 +0100

    Print float64

--- rule/sexpr/print.go
+++ rule/sexpr/print.go
@@ -1,14 +1,28 @@
 package sexpr
 
-import "github.com/heetch/regula/rule"
+import (
+	"strconv"
 
-func Print(e rule.Expr) string {
+	"github.com/heetch/regula/rule"
+)
+
+func Print(e rule.Expr) (string, error) {
 	switch v := e.(type) {
 	case *rule.Value:
 		switch v.Type {
-		case "int64", "foat64":
-			return v.Data
+		case "int64":
+			return v.Data, nil
+		case "float64":
+			// For simplicity we used a fixed precision
+			// representation internally, but that might
+			// pack trailing zeros into its output, so we
+			// go back via a real float64 for printing.
+			f, err := strconv.ParseFloat(v.Data, 64)
+			if err != nil {
+				return "", err
+			}
+			return strconv.FormatFloat(f, 'f', -1, 64), nil
 		}
 	}
-	return ""
+	return "", nil
 }
--- rule/sexpr/print_test.go
+++ rule/sexpr/print_test.go
@@ -19,10 +19,21 @@ func TestPrint(t *testing.T) {
 			input:    rule.Int64Value(590),
 			expected: "590",
 		},
+		{
+			name:     "Negative Int64Value",
+			input:    rule.Int64Value(-590),
+			expected: "-590",
+		},
+		{
+			name:     "Float64Value",
+			input:    rule.Float64Value(72.8987),
+			expected: "72.8987",
+		},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			result := sexpr.Print(c.input)
+			result, err := sexpr.Print(c.input)
+			require.NoError(t, err)
 			require.Equal(t, c.expected, result)
 		})
 	}
