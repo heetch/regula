6feb0d18b509ebeccc25f403a1a834d92f19b807
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-02-26 15:30:26 +0100

    Rewrite Latest to match new store organization

--- store/etcd/get.go
+++ store/etcd/get.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 	"github.com/gogo/protobuf/proto"
+	"github.com/heetch/regula"
 	"github.com/heetch/regula/store"
 	pb "github.com/heetch/regula/store/etcd/proto"
 	"github.com/pkg/errors"
@@ -69,27 +70,42 @@ func (s *RulesetService) Latest(ctx context.Context, path string) (*store.Rulese
 		return nil, store.ErrNotFound
 	}
 
-	resp, err := s.Client.KV.Get(ctx, s.entriesPath(path, "")+versionSeparator, clientv3.WithLastKey()...)
+	// running both requests within a single transaction so only one network round trip is performed.
+	resp, err := s.Client.KV.Txn(ctx).
+		Then(
+			clientv3.OpGet(s.rulesPath(path, "")+versionSeparator, clientv3.WithLastKey()...),
+			clientv3.OpGet(s.signaturesPath(path)),
+			clientv3.OpGet(s.latestVersionPath(path)),
+		).Commit()
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to fetch the entry: %s", path)
+		return nil, errors.Wrapf(err, "failed to fetch ruleset: %s", path)
 	}
 
-	// Count will be 0 if the path doesn't exist or if it's not a ruleset.
-	if resp.Count == 0 {
+	if len(resp.Responses) != 3 {
 		return nil, store.ErrNotFound
 	}
 
-	var entry pb.RulesetEntry
-	err = proto.Unmarshal(resp.Kvs[0].Value, &entry)
+	// decode rules
+	var rules pb.Rules
+	err = proto.Unmarshal(resp.Responses[0].GetResponseRange().Kvs[0].Value, &rules)
 	if err != nil {
-		s.Logger.Debug().Err(err).Bytes("entry", resp.Kvs[0].Value).Msg("latest: unmarshalling failed")
-		return nil, errors.Wrap(err, "failed to unmarshal entry")
+		return nil, errors.Wrap(err, "failed to unmarshal rules")
+	}
+
+	// decode signature
+	var sig pb.Signature
+	err = proto.Unmarshal(resp.Responses[1].GetResponseRange().Kvs[0].Value, &sig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal signature")
 	}
 
 	return &store.RulesetEntry{
-		Path:    entry.Path,
-		Version: entry.Version,
-		Ruleset: rulesetFromProtobuf(entry.Ruleset),
+		Path:    path,
+		Version: string(resp.Responses[2].GetResponseRange().Kvs[0].Value), // decode version
+		Ruleset: &regula.Ruleset{
+			Signature: signatureFromProtobuf(&sig),
+			Rules:     rulesFromProtobuf(&rules),
+		},
 	}, nil
 }
 
