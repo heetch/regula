7e1ef03b42804d2560d532f81ae72ffd4b913a34
Geoffrey J. Teale <tealeg@googlemail.com>
2019-03-04 10:57:53 +0100

    Fix typo on docstring
    Co-Authored-By: asdine <asdine.elhrychy@gmail.com>

--- api/client/rulesets.go
+++ api/client/rulesets.go
@@ -111,7 +111,7 @@ func (s *RulesetService) Put(ctx context.Context, path string, rs *regula.Rulese
 	return &resp, err
 }
 
-// Create creates a ruleset using on the given path using the given signature.
+// Create creates a ruleset at the given path using the given signature.
 func (s *RulesetService) Create(ctx context.Context, path string, sig *regula.Signature) (*api.Ruleset, error) {
 	req, err := s.client.newRequest("POST", s.joinPath(path), sig)
 	if err != nil {
