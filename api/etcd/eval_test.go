880e4a5c7dd77cc149d74a13f5500c5d47a061cc
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-05-07 16:16:59 +0200

    Extract eval tests

---
+++ api/etcd/eval_test.go
@@ -0,0 +1,64 @@
+package etcd
+
+import (
+	"context"
+	"testing"
+
+	"github.com/heetch/regula"
+	"github.com/heetch/regula/errors"
+	"github.com/heetch/regula/rule"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEval(t *testing.T) {
+	t.Parallel()
+
+	s, cleanup := newEtcdRulesetService(t)
+	defer cleanup()
+
+	sig := &regula.Signature{ReturnType: "bool", Params: map[string]string{"id": "string"}}
+	require.NoError(t, s.Create(context.Background(), "a", sig))
+
+	r := rule.New(
+		rule.Eq(
+			rule.StringParam("id"),
+			rule.StringValue("123"),
+		),
+		rule.BoolValue(true),
+	)
+
+	ruleset := createRuleset(t, s, "a", r)
+	version := ruleset.LatestVersion().Version
+
+	t.Run("OK", func(t *testing.T) {
+		res, err := s.Eval(context.Background(), "a", version, regula.Params{
+			"id": "123",
+		})
+		require.NoError(t, err)
+		require.Equal(t, version, res.Version)
+		require.Equal(t, rule.BoolValue(true), res.Value)
+	})
+
+	t.Run("Latest", func(t *testing.T) {
+		res, err := s.Eval(context.Background(), "a", "", regula.Params{
+			"id": "123",
+		})
+		require.NoError(t, err)
+		require.Equal(t, version, res.Version)
+		require.Equal(t, rule.BoolValue(true), res.Value)
+	})
+
+	t.Run("NotFound", func(t *testing.T) {
+		_, err := s.Eval(context.Background(), "b", version, regula.Params{
+			"id": "123",
+		})
+		require.Equal(t, errors.ErrRulesetNotFound, err)
+	})
+
+	t.Run("BadVersion", func(t *testing.T) {
+		_, err := s.Eval(context.Background(), "a", "someversion", regula.Params{
+			"id": "123",
+		})
+		require.Equal(t, errors.ErrRulesetNotFound, err)
+	})
+}
