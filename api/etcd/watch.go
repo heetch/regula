767805a6c65e793884fb949e0c9297af331923ba
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2019-05-21 15:24:10 +0200

    Change etcd watch implementation

--- api/etcd/watch.go
+++ api/etcd/watch.go
@@ -12,8 +12,9 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Watch the given prefix for anything new.
-func (s *RulesetService) Watch(ctx context.Context, prefix string, revision string) (*api.RulesetEvents, error) {
+// Watch a list of paths for changes and return a list of events. If the list is empty or nil,
+// watch all paths. If the revision is empty, watch from the latest revision.
+func (s *RulesetService) Watch(ctx context.Context, paths []string, revision string) (*api.RulesetEvents, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -27,27 +28,42 @@ func (s *RulesetService) Watch(ctx context.Context, prefix string, revision stri
 		Revision: revision,
 	}
 
-	wc := s.Client.Watch(ctx, s.rulesPath(prefix, ""), opts...)
+	wc := s.Client.Watch(ctx, s.rulesPath("", ""), opts...)
 	for {
 		select {
 		case wresp := <-wc:
 			if err := wresp.Err(); err != nil {
-				return nil, errors.Wrapf(err, "failed to watch prefix: '%s'", prefix)
+				return nil, errors.Wrapf(err, "failed to watch paths: '%#v'", paths)
 			}
 
 			if len(wresp.Events) == 0 {
 				continue
 			}
 
-			list := make([]api.RulesetEvent, len(wresp.Events))
+			var list []api.RulesetEvent
 			for i, ev := range wresp.Events {
-				switch ev.Type {
-				case mvccpb.PUT:
-					list[i].Type = api.RulesetPutEvent
-				default:
+				// detect if the event key is found in the paths list
+				// or that the paths list is empty
+				ok := len(paths) == 0
+				for i := 0; i < len(paths) && !ok; i++ {
+					if string(ev.Kv.Key) == s.rulesPath(paths[i], "") {
+						ok = true
+					}
+				}
+
+				// filter keys that haven't been selected
+				if !ok {
+					s.Logger.Debug().Str("type", string(ev.Type)).Str("key", string(ev.Kv.Key)).Msg("watch: ignoring event key")
+					continue
+				}
+
+				// filter event types, keep only PUT events
+				if ev.Type != mvccpb.PUT {
 					s.Logger.Debug().Str("type", string(ev.Type)).Msg("watch: ignoring event type")
 					continue
 				}
+
+				list[i].Type = api.RulesetPutEvent
 
 				var pbrs pb.Rules
 				err := proto.Unmarshal(ev.Kv.Value, &pbrs)
